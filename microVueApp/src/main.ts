cd5bd0fb1489e7d0a9130d855a9fa255dca12fab
dshuais <1137896420@qq.com>
2024-07-23 23:24:26 +0800

    ✨ feat: 调通vue子应用

--- microVueApp/src/main.ts
+++ microVueApp/src/main.ts
@@ -1,5 +1,41 @@
 import { createApp } from 'vue'
+import { renderWithQiankun, qiankunWindow, QiankunProps } from 'vite-plugin-qiankun/dist/helper'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const initQiankun = () => {
+  renderWithQiankun({
+    mount(props: QiankunProps) {
+
+      render(props.container);
+      
+      props.onGlobalStateChange((res: unknown) => {
+        console.log('micro vueApp onGlobalStateChange:>> ', res);
+      })
+    },
+
+    update(props) {
+      console.log('micro vueApp update:>> ', props);
+    },
+
+    bootstrap() {
+      console.log('micro vueApp bootstrap');
+    },
+
+    unmount(props) {
+      console.log('micro vueApp unmount:>> ', props);
+    }
+  })
+}
+
+const render = (container?: HTMLElement) => {
+  // 如果是在主应用的环境下就挂载主应用的节点，否则挂载到本地
+  const appDom = container ? container : '#app';
+
+  createApp(App).mount(appDom);
+}
+
+console.log('microVueApp是否在qiankun:>> ', qiankunWindow.__POWERED_BY_QIANKUN__);
+
+// 判断当前应用是否在主应用中
+qiankunWindow.__POWERED_BY_QIANKUN__ ? initQiankun() : render()
--- microVueApp/vite.config.ts
+++ microVueApp/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import qiankun from 'vite-plugin-qiankun'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+    qiankun('microVueApp', {
+      useDevMode: true
+    })
+  ],
+
+  server: {
+    port: 3002
+  }
 })
