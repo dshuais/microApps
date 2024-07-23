f97063895075ee2de84afc08b1e65a39ae427003
dshuais <1137896420@qq.com>
2024-07-23 23:08:08 +0800

    ✨ feat: 调通react子应用

--- microReactApp/src/main.tsx
+++ microReactApp/src/main.tsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { renderWithQiankun, qiankunWindow, QiankunProps } from 'vite-plugin-qiankun/dist/helper'
+
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const initQiankun = () => {
+  renderWithQiankun({
+    mount(props: QiankunProps) {
+
+      render(props.container);
+      
+      props.onGlobalStateChange((res: unknown) => {
+        console.log('micro reactApp onGlobalStateChange:>> ', res);
+      })
+    },
+
+    update(props) {
+      console.log('micro reactApp update:>> ', props);
+    },
+
+    bootstrap() {
+      console.log('micro reactApp bootstrap');
+    },
+
+    unmount(props) {
+      console.log('micro reactApp unmount:>> ', props);
+    }
+  })
+}
+
+const render = (container?: HTMLElement) => {
+  // 如果是在主应用的环境下就挂载主应用的节点，否则挂载到本地
+  const appDom = container ? container : document.getElementById('root')
+
+  ReactDOM.createRoot(appDom!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}
+
+console.log('是否在qiankun:>> ', qiankunWindow.__POWERED_BY_QIANKUN__);
+
+
+// 判断当前应用是否在主应用中
+qiankunWindow.__POWERED_BY_QIANKUN__ ? initQiankun() : render()
--- microReactApp/vite.config.ts
+++ microReactApp/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import qiankun from 'vite-plugin-qiankun'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    // react(),
+
+    qiankun('microReactApp', {
+      useDevMode: true
+    })
+  ],
 
   server: {
     port: 3001
